alainm23/planify

commit 7115e6a578f997ff6d0fd55b59550bd937364ae4

fix: use UTC timezone for COMPLETED property in VTODO (#2490)
* use timezone

* update new

* fix ci

core/Objects/Item.vala
@@ -1365,7 +1365,7 @@ public class Objects.Item : Objects.BaseObject {
             ical.add_property (new ICal.Property.percentcomplete (100));
             // RFC requires Date-Time (https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.2.1)
             // Nextcloud also accepted .today () which didn't include the Timezone, but Radicale and probably other CalDAV implementations want Date-Time
-            ical.add_property (new ICal.Property.completed (new ICal.Time.current_with_zone (null)));
+            ical.add_property (new ICal.Property.completed (new ICal.Time.from_timet_with_zone ((time_t) new GLib.DateTime.now_utc ().to_unix (), 0, ICal.Timezone.get_utc_timezone ())));
         } else {
             ical.set_status (ICal.PropertyStatus.NEEDSACTION);
         }

commit c4f6c93ed743b83a4aa1d9cd17014fc1e6044e19

Fix building with libical4
It broke in commit 7115e6a578f997ff6d0fd55b59550bd937364ae4

core/Objects/Item.vala
@@ -1365,7 +1365,11 @@ public class Objects.Item : Objects.BaseObject {
             ical.add_property (new ICal.Property.percentcomplete (100));
             // RFC requires Date-Time (https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.2.1)
             // Nextcloud also accepted .today () which didn't include the Timezone, but Radicale and probably other CalDAV implementations want Date-Time
+            #if IS_LIBICAL4
+            ical.add_property (new ICal.Property.completed (new ICal.Time.from_timet_with_zone ((time_t) new GLib.DateTime.now_utc ().to_unix (), false, ICal.Timezone.get_utc_timezone ())));
+            #else
             ical.add_property (new ICal.Property.completed (new ICal.Time.from_timet_with_zone ((time_t) new GLib.DateTime.now_utc ().to_unix (), 0, ICal.Timezone.get_utc_timezone ())));
+            #endif
         } else {
             ical.set_status (ICal.PropertyStatus.NEEDSACTION);
         }